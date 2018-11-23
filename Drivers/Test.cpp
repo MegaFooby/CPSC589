af2b831eaf63cbc91e122f682b44243fc1dd7200
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2018-11-22 21:18:11 -0700

    Add files via upload

---
+++ Drivers/Test.cpp
@@ -0,0 +1,578 @@
+#include <bitset>
+#include <random>
+#include <string.h>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+//#include "hidapi.c"
+
+#include "packet.h"
+#include "Joycon.hpp"
+#include "tools.hpp"
+
+
+#define JOYCON_VENDOR 0x057e
+#define JOYCON_L_BT 0x2006
+#define JOYCON_R_BT 0x2007
+#define PRO_CONTROLLER 0x2009
+#define JOYCON_CHARGING_GRIP 0x200e
+#define SERIAL_LEN 18
+#define PI 3.14159265359
+#define L_OR_R(lr) (lr == 1 ? 'L' : (lr == 2 ? 'R' : '?'))
+
+std::vector<Joycon> joycons;
+unsigned char buf[65];
+int res = 0;
+
+using namespace std;
+
+
+struct Tracker {
+
+	int var1 = 0;
+	int var2 = 0;
+	int counter1 = 0;
+
+	float low_freq = 200.0f;
+	float high_freq = 500.0f;
+
+	float relX = 0;
+	float relY = 0;
+
+	float anglex = 0;
+	float angley = 0;
+	float anglez = 0;
+
+	//glm::fquat quat = glm::angleAxis(0.0f, glm::vec3(1.0, 0.0, 0.0));
+
+	vector<chrono::high_resolution_clock::time_point> tPolls;
+
+	float previousPitch = 0;
+
+} tracker;
+
+
+void handle_input(Joycon *jc, uint8_t *packet, int len) {
+
+	// bluetooth button pressed packet:
+	if (packet[0] == 0x3F) {
+
+		uint16_t old_buttons = jc->buttons;
+		int8_t old_dstick = jc->dstick;
+
+		jc->dstick = packet[3];
+		// todo: get button states here aswell:
+	}
+
+	// input update packet:
+	// 0x21 is just buttons, 0x30 includes gyro, 0x31 includes NFC (large packet size)
+	if (packet[0] == 0x21 || packet[0] == 0x30 || packet[0] == 0x31) {
+
+		// offset for usb or bluetooth data:
+		/*int offset = settings.usingBluetooth ? 0 : 10;*/
+		int offset = jc->bluetooth ? 0 : 10;
+
+		uint8_t *btn_data = packet + offset + 3;
+
+		// get button states:
+		{
+			uint16_t states = 0;
+			uint16_t states2 = 0;
+
+			// Left JoyCon:
+			if (jc->left_right == 1) {
+				states = (btn_data[1] << 8) | (btn_data[2] & 0xFF);
+				// Right JoyCon:
+			} else if (jc->left_right == 2) {
+				states = (btn_data[1] << 8) | (btn_data[0] & 0xFF);
+				// Pro Controller:
+			} else if (jc->left_right == 3) {
+				states = (btn_data[1] << 8) | (btn_data[2] & 0xFF);
+				states2 = (btn_data[1] << 8) | (btn_data[0] & 0xFF);
+			}
+
+			jc->buttons = states;
+			// Pro Controller:
+			if (jc->left_right == 3) {
+				jc->buttons2 = states2;
+
+				// fix some non-sense the Pro Controller does
+				// clear nth bit
+				//num &= ~(1UL << n);
+				jc->buttons &= ~(1UL << 9);
+				jc->buttons &= ~(1UL << 10);
+				jc->buttons &= ~(1UL << 12);
+				jc->buttons &= ~(1UL << 14);
+
+				jc->buttons2 &= ~(1UL << 8);
+				jc->buttons2 &= ~(1UL << 11);
+				jc->buttons2 &= ~(1UL << 13);
+			}
+		}
+
+		// get stick data:
+		uint8_t *stick_data = packet + offset;
+		if (jc->left_right == 1) {
+			stick_data += 6;
+		} else if (jc->left_right == 2) {
+			stick_data += 9;
+		}
+
+		uint16_t stick_x = stick_data[0] | ((stick_data[1] & 0xF) << 8);
+		uint16_t stick_y = (stick_data[1] >> 4) | (stick_data[2] << 4);
+		jc->stick.x = stick_x;
+		jc->stick.y = stick_y;
+
+		// use calibration data:
+		jc->CalcAnalogStick();
+
+		// pro controller:
+		if (jc->left_right == 3) {
+			stick_data += 6;
+			uint16_t stick_x = stick_data[0] | ((stick_data[1] & 0xF) << 8);
+			uint16_t stick_y = (stick_data[1] >> 4) | (stick_data[2] << 4);
+			jc->stick.x = (int)(unsigned int)stick_x;
+			jc->stick.y = (int)(unsigned int)stick_y;
+			stick_data += 3;
+			uint16_t stick_x2 = stick_data[0] | ((stick_data[1] & 0xF) << 8);
+			uint16_t stick_y2 = (stick_data[1] >> 4) | (stick_data[2] << 4);
+			jc->stick2.x = (int)(unsigned int)stick_x2;
+			jc->stick2.y = (int)(unsigned int)stick_y2;
+
+			// calibration data:
+			jc->CalcAnalogStick();
+		}
+
+		jc->battery = (stick_data[1] & 0xF0) >> 4;
+		//printf("JoyCon battery: %d\n", jc->battery);
+
+		// Accelerometer:
+		// Accelerometer data is absolute (m/s^2)
+		{
+
+			// get accelerometer X:
+			jc->accel.x = (float)(uint16_to_int16(packet[13] | (packet[14] << 8) & 0xFF00)) * jc->acc_cal_coeff[0];
+
+			// get accelerometer Y:
+			jc->accel.y = (float)(uint16_to_int16(packet[15] | (packet[16] << 8) & 0xFF00)) * jc->acc_cal_coeff[1];
+
+			// get accelerometer Z:
+			jc->accel.z = (float)(uint16_to_int16(packet[17] | (packet[18] << 8) & 0xFF00)) * jc->acc_cal_coeff[2];
+		}
+
+
+
+		// Gyroscope:
+		// Gyroscope data is relative (rads/s)
+		{
+
+			// get roll:
+			jc->gyro.roll = (float)((uint16_to_int16(packet[19] | (packet[20] << 8) & 0xFF00)) - jc->sensor_cal[1][0]) * jc->gyro_cal_coeff[0];
+
+			// get pitch:
+			jc->gyro.pitch = (float)((uint16_to_int16(packet[21] | (packet[22] << 8) & 0xFF00)) - jc->sensor_cal[1][1]) * jc->gyro_cal_coeff[1];
+
+			// get yaw:
+			jc->gyro.yaw = (float)((uint16_to_int16(packet[23] | (packet[24] << 8) & 0xFF00)) - jc->sensor_cal[1][2]) * jc->gyro_cal_coeff[2];
+		}
+
+		// offsets:
+		{
+			jc->setGyroOffsets();
+
+			jc->gyro.roll -= jc->gyro.offset.roll;
+			jc->gyro.pitch -= jc->gyro.offset.pitch;
+			jc->gyro.yaw -= jc->gyro.offset.yaw;
+
+			//tracker.counter1++;
+			//if (tracker.counter1 > 10) {
+			//	tracker.counter1 = 0;
+			//	printf("%.3f %.3f %.3f\n", abs(jc->gyro.roll), abs(jc->gyro.pitch), abs(jc->gyro.yaw));
+			//}
+		}
+
+
+		//hex_dump(gyro_data, 20);
+
+		if (jc->left_right == 1) {
+			//hex_dump(gyro_data, 20);
+			//hex_dump(packet+12, 20);
+			//printf("x: %f, y: %f, z: %f\n", tracker.anglex, tracker.angley, tracker.anglez);
+			//printf("%04x\n", jc->stick.x);
+			//printf("%f\n", jc->stick.CalX);
+			//printf("%d\n", jc->gyro.yaw);
+			//printf("%02x\n", jc->gyro.roll);
+			//printf("%04x\n", jc->gyro.yaw);
+			//printf("%04x\n", jc->gyro.roll);
+			//printf("%f\n", jc->gyro.roll);
+			//printf("%d\n", accelXA);
+			//printf("%d\n", jc->buttons);
+			//printf("%.4f\n", jc->gyro.pitch);
+			//printf("%04x\n", accelX);
+			//printf("%02x %02x\n", rollA, rollB);
+		}
+
+	}
+
+
+
+
+
+
+	// handle button combos:
+	{
+
+		// press up, down, left, right, L, ZL to restart:
+		if (jc->left_right == 1) {
+			//if (jc->buttons == 207) {
+			//	settings.restart = true;
+			//}
+
+			// remove this, it's just for rumble testing
+			//uint8_t hfa2 = 0x88;
+			//uint16_t lfa2 = 0x804d;
+
+			//tracker.low_freq = clamp(tracker.low_freq, 41.0f, 626.0f);
+			//tracker.high_freq = clamp(tracker.high_freq, 82.0f, 1252.0f);
+			//
+			//// down:
+			//if (jc->buttons == 1) {
+			//	tracker.high_freq -= 1;
+			//	jc->rumble4(tracker.low_freq, tracker.high_freq, hfa2, lfa2);
+			//}
+			//// down:
+			//if (jc->buttons == 2) {
+			//	tracker.high_freq += 1;
+			//	jc->rumble4(tracker.low_freq, tracker.high_freq, hfa2, lfa2);
+			//}
+
+			//// left:
+			//if (jc->buttons == 8) {
+			//	tracker.low_freq -= 1;
+			//	jc->rumble4(tracker.low_freq, tracker.high_freq, hfa2, lfa2);
+			//}
+			//// right:
+			//if (jc->buttons == 4) {
+			//	tracker.low_freq += 1;
+			//	jc->rumble4(tracker.low_freq, tracker.high_freq, hfa2, lfa2);
+			//}
+
+			////printf("%i\n", jc->buttons);
+			////printf("%f\n", tracker.frequency);
+			//printf("%f %f\n", tracker.low_freq, tracker.high_freq);
+		}
+
+
+		// left:
+		if (jc->left_right == 1) {
+			jc->btns.down = (jc->buttons & (1 << 0)) ? 1 : 0;
+			jc->btns.up = (jc->buttons & (1 << 1)) ? 1 : 0;
+			jc->btns.right = (jc->buttons & (1 << 2)) ? 1 : 0;
+			jc->btns.left = (jc->buttons & (1 << 3)) ? 1 : 0;
+			jc->btns.sr = (jc->buttons & (1 << 4)) ? 1 : 0;
+			jc->btns.sl = (jc->buttons & (1 << 5)) ? 1 : 0;
+			jc->btns.l = (jc->buttons & (1 << 6)) ? 1 : 0;
+			jc->btns.zl = (jc->buttons & (1 << 7)) ? 1 : 0;
+			jc->btns.minus = (jc->buttons & (1 << 8)) ? 1 : 0;
+			jc->btns.stick_button = (jc->buttons & (1 << 11)) ? 1 : 0;
+			jc->btns.capture = (jc->buttons & (1 << 13)) ? 1 : 0;
+
+
+			if (/*settings.debugMode*/false) {
+				printf("U: %d D: %d L: %d R: %d LL: %d ZL: %d SB: %d SL: %d SR: %d M: %d C: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+					jc->btns.up, jc->btns.down, jc->btns.left, jc->btns.right, jc->btns.l, jc->btns.zl, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+					jc->btns.minus, jc->btns.capture, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+			}
+			//if (settings.writeDebugToFile) {
+			//	fprintf(settings.outputFile, "U: %d D: %d L: %d R: %d LL: %d ZL: %d SB: %d SL: %d SR: %d M: %d C: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+			//		jc->btns.up, jc->btns.down, jc->btns.left, jc->btns.right, jc->btns.l, jc->btns.zl, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+			//		jc->btns.minus, jc->btns.capture, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+			//}
+		}
+
+		// right:
+		if (jc->left_right == 2) {
+			jc->btns.y = (jc->buttons & (1 << 0)) ? 1 : 0;
+			jc->btns.x = (jc->buttons & (1 << 1)) ? 1 : 0;
+			jc->btns.b = (jc->buttons & (1 << 2)) ? 1 : 0;
+			jc->btns.a = (jc->buttons & (1 << 3)) ? 1 : 0;
+			jc->btns.sr = (jc->buttons & (1 << 4)) ? 1 : 0;
+			jc->btns.sl = (jc->buttons & (1 << 5)) ? 1 : 0;
+			jc->btns.r = (jc->buttons & (1 << 6)) ? 1 : 0;
+			jc->btns.zr = (jc->buttons & (1 << 7)) ? 1 : 0;
+			jc->btns.plus = (jc->buttons & (1 << 9)) ? 1 : 0;
+			jc->btns.stick_button = (jc->buttons & (1 << 10)) ? 1 : 0;
+			jc->btns.home = (jc->buttons & (1 << 12)) ? 1 : 0;
+
+
+			if (/*settings.debugMode*/false) {
+				printf("A: %d B: %d X: %d Y: %d RR: %d ZR: %d SB: %d SL: %d SR: %d P: %d H: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+					jc->btns.a, jc->btns.b, jc->btns.x, jc->btns.y, jc->btns.r, jc->btns.zr, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+					jc->btns.plus, jc->btns.home, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+			}
+			//if (settings.writeDebugToFile) {
+			//	fprintf(settings.outputFile, "A: %d B: %d X: %d Y: %d RR: %d ZR: %d SB: %d SL: %d SR: %d P: %d H: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+			//		jc->btns.a, jc->btns.b, jc->btns.x, jc->btns.y, jc->btns.r, jc->btns.zr, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+			//		jc->btns.plus, jc->btns.home, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+			//}
+		}
+
+		// pro controller:
+		if (jc->left_right == 3) {
+
+			// left:
+			jc->btns.down = (jc->buttons & (1 << 0)) ? 1 : 0;
+			jc->btns.up = (jc->buttons & (1 << 1)) ? 1 : 0;
+			jc->btns.right = (jc->buttons & (1 << 2)) ? 1 : 0;
+			jc->btns.left = (jc->buttons & (1 << 3)) ? 1 : 0;
+			jc->btns.sr = (jc->buttons & (1 << 4)) ? 1 : 0;
+			jc->btns.sl = (jc->buttons & (1 << 5)) ? 1 : 0;
+			jc->btns.l = (jc->buttons & (1 << 6)) ? 1 : 0;
+			jc->btns.zl = (jc->buttons & (1 << 7)) ? 1 : 0;
+			jc->btns.minus = (jc->buttons & (1 << 8)) ? 1 : 0;
+			jc->btns.stick_button = (jc->buttons & (1 << 11)) ? 1 : 0;
+			jc->btns.capture = (jc->buttons & (1 << 13)) ? 1 : 0;
+
+			// right:
+			jc->btns.y = (jc->buttons2 & (1 << 0)) ? 1 : 0;
+			jc->btns.x = (jc->buttons2 & (1 << 1)) ? 1 : 0;
+			jc->btns.b = (jc->buttons2 & (1 << 2)) ? 1 : 0;
+			jc->btns.a = (jc->buttons2 & (1 << 3)) ? 1 : 0;
+			jc->btns.sr = (jc->buttons2 & (1 << 4)) ? 1 : 0;
+			jc->btns.sl = (jc->buttons2 & (1 << 5)) ? 1 : 0;
+			jc->btns.r = (jc->buttons2 & (1 << 6)) ? 1 : 0;
+			jc->btns.zr = (jc->buttons2 & (1 << 7)) ? 1 : 0;
+			jc->btns.plus = (jc->buttons2 & (1 << 9)) ? 1 : 0;
+			jc->btns.stick_button2 = (jc->buttons2 & (1 << 10)) ? 1 : 0;
+			jc->btns.home = (jc->buttons2 & (1 << 12)) ? 1 : 0;
+
+
+			if (/*settings.debugMode*/false) {
+
+				printf("U: %d D: %d L: %d R: %d LL: %d ZL: %d SB: %d SL: %d SR: %d M: %d C: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+					jc->btns.up, jc->btns.down, jc->btns.left, jc->btns.right, jc->btns.l, jc->btns.zl, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+					jc->btns.minus, jc->btns.capture, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+
+				printf("A: %d B: %d X: %d Y: %d RR: %d ZR: %d SB: %d SL: %d SR: %d P: %d H: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+					jc->btns.a, jc->btns.b, jc->btns.x, jc->btns.y, jc->btns.r, jc->btns.zr, jc->btns.stick_button2, jc->btns.sl, jc->btns.sr, \
+					jc->btns.plus, jc->btns.home, (jc->stick2.CalX + 1), (jc->stick2.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+			}
+
+			//if (settings.writeDebugToFile) {
+			//	fprintf(settings.outputFile, "U: %d D: %d L: %d R: %d LL: %d ZL: %d SB: %d SL: %d SR: %d M: %d C: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+			//		jc->btns.up, jc->btns.down, jc->btns.left, jc->btns.right, jc->btns.l, jc->btns.zl, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+			//		jc->btns.minus, jc->btns.capture, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+
+			//	fprintf(settings.outputFile, "A: %d B: %d X: %d Y: %d RR: %d ZR: %d SB: %d SL: %d SR: %d P: %d H: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+			//		jc->btns.a, jc->btns.b, jc->btns.x, jc->btns.y, jc->btns.r, jc->btns.zr, jc->btns.stick_button2, jc->btns.sl, jc->btns.sr, \
+			//		jc->btns.plus, jc->btns.home, (jc->stick2.CalX + 1), (jc->stick2.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+			//}
+
+		}
+
+	}
+}
+
+
+void pollLoop() {
+
+	// poll joycons:
+	for (int i = 0; i < joycons.size(); ++i) {
+
+		Joycon *jc = &joycons[i];
+
+		if (!jc->handle) { continue; }
+
+		if (/*settings.forcePollUpdate*/false) {
+			// set to be blocking:
+			hid_set_nonblocking(jc->handle, 0);
+		} else {
+			// set to be non-blocking:
+			hid_set_nonblocking(jc->handle, 1);
+		}
+
+		// get input:
+		memset(buf, 0, 65);
+
+		// get current time
+		chrono::high_resolution_clock::time_point tNow = chrono::high_resolution_clock::now();
+
+		auto timeSincePoll = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tracker.tPolls[i]);
+
+		// time spent sleeping (0):
+		double timeSincePollMS = timeSincePoll.count() / 1000.0;
+
+		if (timeSincePollMS > (1000.0 / /*settings.pollsPerSec*/60.0f)) {
+			jc->send_command(0x1E, buf, 0);
+			tracker.tPolls[i] = chrono::high_resolution_clock::now();
+		}
+
+
+		hid_read(jc->handle, buf, 0x40);
+
+		// get rid of queue:
+		// if we force the poll to wait then the queue will never clear and will just freeze:
+		if (/*!settings.forcePollUpdate*/true) {
+			while (hid_read(jc->handle, buf, 0x40) > 0) {};
+		}
+
+		handle_input(jc, buf, 0x40);
+	}
+
+	// DO STUFF WITH JOYCONS HERE:
+
+	// get first connected joycon:
+	Joycon *jc = &joycons[0];
+
+	// left joycon:
+					printf("U: %d D: %d L: %d R: %d LL: %d ZL: %d SB: %d SL: %d SR: %d M: %d C: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+					jc->btns.up, jc->btns.down, jc->btns.left, jc->btns.right, jc->btns.l, jc->btns.zl, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+					jc->btns.minus, jc->btns.capture, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+
+	// right joycon:
+	//				printf("A: %d B: %d X: %d Y: %d RR: %d ZR: %d SB: %d SL: %d SR: %d P: %d H: %d SX: %.5f SY: %.5f GR: %06d GP: %06d GY: %06d\n", \
+					jc->btns.a, jc->btns.b, jc->btns.x, jc->btns.y, jc->btns.r, jc->btns.zr, jc->btns.stick_button, jc->btns.sl, jc->btns.sr, \
+					jc->btns.plus, jc->btns.home, (jc->stick.CalX + 1), (jc->stick.CalY + 1), (int)jc->gyro.roll, (int)jc->gyro.pitch, (int)jc->gyro.yaw);
+
+	// sleep:
+	usleep(2000);// 8.00
+}
+
+void start() {
+
+	int read;	// number of bytes read
+	int written;// number of bytes written
+	const char *device_name;
+
+	// Enumerate and print the HID devices on the system
+	struct hid_device_info *devs, *cur_dev;
+
+	res = hid_init();
+
+	// hack:
+	for (int i = 0; i < 100; ++i) {
+		tracker.tPolls.push_back(std::chrono::high_resolution_clock::now());
+	}
+
+
+	if (/*settings.writeDebugToFile*/false) {
+
+		// find a debug file to output to:
+		int fileNumber = 0;
+		std::string name = std::string("output-") + std::to_string(fileNumber) + std::string(".txt");
+		while (exists_test0(name)) {
+			fileNumber += 1;
+			name = std::string("output-") + std::to_string(fileNumber) + std::string(".txt");
+		}
+
+		//settings.outputFile = fopen(name.c_str(), "w");
+	}
+
+
+init_start:
+
+	devs = hid_enumerate(JOYCON_VENDOR, 0x0);
+	cur_dev = devs;
+	while (cur_dev) {
+
+		// identify by vendor:
+		if (cur_dev->vendor_id == JOYCON_VENDOR) {
+
+			// bluetooth, left / right joycon:
+			if (cur_dev->product_id == JOYCON_L_BT || cur_dev->product_id == JOYCON_R_BT) {
+				Joycon jc = Joycon(cur_dev);
+				joycons.push_back(jc);
+			}
+
+			// pro controller:
+			if (cur_dev->product_id == PRO_CONTROLLER) {
+				Joycon jc = Joycon(cur_dev);
+				joycons.push_back(jc);
+			}
+
+			// charging grip:
+			//if (cur_dev->product_id == JOYCON_CHARGING_GRIP) {
+			//	Joycon jc = Joycon(cur_dev);
+			//	settings.usingBluetooth = false;
+			//	settings.combineJoyCons = true;
+			//	joycons.push_back(jc);
+			//}
+
+		}
+
+
+		cur_dev = cur_dev->next;
+	}
+	hid_free_enumeration(devs);
+
+
+
+	// init joycons:
+	if (/*settings.usingGrip*/false) {
+		for (int i = 0; i < joycons.size(); ++i) {
+			joycons[i].init_usb();
+		}
+	} else {
+		for (int i = 0; i < joycons.size(); ++i) {
+			joycons[i].init_bt();
+		}
+	}
+
+	// initial poll to get battery data:
+	pollLoop();
+	for (int i = 0; i < joycons.size(); ++i) {
+		printf("battery level: %u\n", joycons[i].battery);
+	}
+
+	// set lights:
+	printf("setting LEDs...\n");
+	for (int r = 0; r < 5; ++r) {
+		for (int i = 0; i < joycons.size(); ++i) {
+			Joycon *jc = &joycons[i];
+			// Player LED Enable
+			memset(buf, 0x00, 0x40);
+			if (i == 0) {
+				buf[0] = 0x0 | 0x0 | 0x0 | 0x1;		// solid 1
+			}
+			if (i == 1) {
+				if (/*settings.combineJoyCons*/true) {
+					buf[0] = 0x0 | 0x0 | 0x0 | 0x1; // solid 1
+				} else if (/*!settings.combineJoyCons*/false) {
+					buf[0] = 0x0 | 0x0 | 0x2 | 0x0; // solid 2
+				}
+			}
+			//buf[0] = 0x80 | 0x40 | 0x2 | 0x1; // Flash top two, solid bottom two
+			//buf[0] = 0x8 | 0x4 | 0x2 | 0x1; // All solid
+			//buf[0] = 0x80 | 0x40 | 0x20 | 0x10; // All flashing
+			//buf[0] = 0x80 | 0x00 | 0x20 | 0x10; // All flashing except 3rd light (off)
+			jc->send_subcommand(0x01, 0x30, buf, 1);
+		}
+	}
+
+
+	// give a small rumble to all joycons:
+	printf("vibrating JoyCon(s).\n");
+	for (int k = 0; k < 1; ++k) {
+		for (int i = 0; i < joycons.size(); ++i) {
+			joycons[i].rumble(100, 1);
+			usleep(20000);
+			joycons[i].rumble(10, 3);
+		}
+	}
+
+	printf("Done.\n");
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+	start();
+	while (true) {
+		pollLoop();
+	}
+	return 0;
+}
---
+++ Drivers/tools.hpp
@@ -0,0 +1,376 @@
+#pragma once
+#include <chrono>
+#include <thread>
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+//#include <curl/curl.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+
+double lowpassFilter(double a, double thresh) {
+	if (std::abs(a) > thresh) {
+		return a;
+	} else {
+		return 0;
+	}
+}
+
+int rand_range(int min, int max) {
+	return rand() % (max + 1 - min) + min;
+}
+
+// sleeps accurately:
+/*void accurateSleep(double durationMS, double sleepThreshold = 1.8) {
+
+	// get current time
+	auto tNow = std::chrono::high_resolution_clock::now();
+
+	auto tSleepStart = std::chrono::high_resolution_clock::now();
+
+	auto tSleepDuration = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tSleepStart);
+
+	// get the application's runtime duration in ms
+	//runningTimeMS = std::chrono::duration_cast<std::chrono::milliseconds>(tNow - tApplicationStart).count();
+	//auto tFrameDuration = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tFrameStart);
+	//double tFrameDurationMS = tFrameDuration.count() / 1000.0;
+
+
+	// time spent sleeping (0):
+	double tSleepTimeMS = tSleepDuration.count() / 1000.0;
+
+	//float lowerThres = 0.2;
+	//float sleepThreshold = 1.8;//1.4
+
+	// run cpu in circles
+	while (tSleepTimeMS < durationMS) {
+		// allow cpu to sleep if there is lots of time to kill
+		if (tSleepTimeMS < durationMS - sleepThreshold) {
+			std::this_thread::sleep_for(std::chrono::microseconds(100));
+			//Sleep(1);
+		}
+		tNow = std::chrono::high_resolution_clock::now();
+		tSleepDuration = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tSleepStart);
+		tSleepTimeMS = tSleepDuration.count() / 1000.0;
+	}
+
+	// done sleeping
+
+}*/
+
+// sleeps very accurately:
+void veryAccurateSleep(double durationMS) {
+
+	// get current time
+	auto tNow = std::chrono::high_resolution_clock::now();
+
+	auto tSleepStart = std::chrono::high_resolution_clock::now();
+
+	auto tSleepDuration = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tSleepStart);
+
+	// get the application's runtime duration in ms
+	//runningTimeMS = std::chrono::duration_cast<std::chrono::milliseconds>(tNow - tApplicationStart).count();
+	//auto tFrameDuration = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tFrameStart);
+	//double tFrameDurationMS = tFrameDuration.count() / 1000.0;
+
+
+	// time spent sleeping (0):
+	double tSleepTimeMS = tSleepDuration.count() / 1000.0;
+
+	//float lowerThres = 0.2;
+	//float sleepThreshold = 1.8;//1.4
+
+	// run cpu in circles
+	while (tSleepTimeMS < durationMS) {
+		
+		// never sleep, burn the CPU
+		tNow = std::chrono::high_resolution_clock::now();
+		tSleepDuration = std::chrono::duration_cast<std::chrono::microseconds>(tNow - tSleepStart);
+		tSleepTimeMS = tSleepDuration.count() / 1000.0;
+	}
+
+	// done "sleeping"
+}
+
+
+
+/* The LoadConfig function loads the configuration file given by filename
+It returns a map of key-value pairs stored in the conifuration file */
+std::map<std::string, std::string> LoadConfig(std::string filename)
+{
+	std::ifstream input(filename); //The input stream
+	std::map<std::string, std::string> ans; //A map of key-value pairs in the file
+	while (input) //Keep on going as long as the file stream is good
+	{
+		std::string key; //The key
+		std::string value; //The value
+		std::getline(input, key, ':'); //Read up to the : delimiter into key
+		std::getline(input, value, '\n'); //Read up to the newline into value
+		std::string::size_type pos1 = value.find_first_of("\""); //Find the first quote in the value
+		std::string::size_type pos2 = value.find_last_of("\""); //Find the last quote in the value
+		if (pos1 != std::string::npos && pos2 != std::string::npos && pos2 > pos1) //Check if the found positions are all valid
+		{
+			value = value.substr(pos1 + 1, pos2 - pos1 - 1); //Take a substring of the part between the quotes
+			ans[key] = value; //Store the result in the map
+		}
+	}
+	input.close(); //Close the file stream
+	return ans; //And return the result
+}
+
+
+/*void setupConsole(std::string title) {
+	// setup console
+	AllocConsole();
+	freopen("conin$", "r", stdin);
+	freopen("conout$", "w", stdout);
+	freopen("conout$", "w", stderr);
+	printf("Debugging Window:\n");
+}*/
+
+
+int16_t unsignedToSigned16(uint16_t n) {
+	uint16_t A = n;
+	uint16_t B = 0xFFFF - A;
+	if (A < B) {
+		return (int16_t)A;
+	} else {
+		return (int16_t)(-1 * B);
+	}
+}
+
+int16_t uint16_to_int16(uint16_t a) {
+	int16_t b;
+	char* aPointer = (char*)&a, *bPointer = (char*)&b;
+	memcpy(bPointer, aPointer, sizeof(a));
+	return b;
+}
+
+uint16_t combine_uint8_t(uint8_t a, uint8_t b) {
+	uint16_t c = ((uint16_t)a << 8) | b;
+	return c;
+}
+
+int16_t combine_gyro_data(uint8_t a, uint8_t b) {
+	uint16_t c = combine_uint8_t(a, b);
+	int16_t d = uint16_to_int16(c);
+	return d;
+}
+
+
+float clamp(float a, float min, float max) {
+	if (a < min) {
+		return min;
+	} else if (a > max) {
+		return max;
+	} else {
+		return a;
+	}
+}
+
+unsigned createMask(unsigned a, unsigned b) {
+	unsigned r = 0;
+	for (unsigned i = a; i <= b; i++)
+		r |= 1 << i;
+
+	return r;
+}
+
+inline int mk_even(int n) {
+	return n - n % 2;
+}
+
+inline int mk_odd(int n) {
+	return n - (n % 2 ? 0 : 1);
+}
+
+
+float rand0t1() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(0.0f, 1.0f);
+	float rnd = dis(gen);
+	return rnd;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct s_button_map {
+//	int bit;
+//	char *name;
+//};
+//
+//
+//struct s_button_map button_map[16] = {
+//	{ 0, "D" },{ 1, "R" },{ 2, "L" },{ 3, "U" },{ 4, "SL" },{ 5, "SR" },
+//	{ 6, "?" },{ 7, "?" },{ 8, "-" },{ 9, "+" },{ 10, "LS" },{ 11, "RS" },
+//	{ 12, "Ho" },{ 13, "Sc" },{ 14, "LR" },{ 15, "ZLR" },
+//};
+//
+//void print_buttons(Joycon *jc) {
+//
+//	for (int i = 0; i < 16; i++) {
+//		if (jc->buttons & (1 << button_map[i].bit)) {
+//			printf("1");
+//		} else {
+//			printf("0");
+//		}
+//	}
+//	printf("\n");
+//}
+
+
+//void print_buttons2(Joycon *jc) {
+//
+//	printf("Joycon %c (Unattached): ", L_OR_R(jc->left_right));
+//
+//	for (int i = 0; i < 32; i++) {
+//		if (jc->buttons2[i]) {
+//			printf("1");
+//		} else {
+//			printf("0");
+//		}
+//
+//	}
+//	printf("\n");
+//}
+
+//void print_stick2(Joycon *jc) {
+//
+//	printf("Joycon %c (Unattached): ", L_OR_R(jc->left_right));
+//
+//	printf("%d %d\n", jc->stick.horizontal, jc->stick.vertical);
+//}
+
+
+
+
+const char *const dstick_names[9] = { "Up", "UR", "Ri", "DR", "Do", "DL", "Le", "UL", "Neu" };
+
+
+
+void hex_dump(unsigned char *buf, int len) {
+	for (int i = 0; i < len; i++) {
+		printf("%02x ", buf[i]);
+	}
+	printf("\n");
+}
+
+void hex_dump2(unsigned char *buf, int len) {
+	for (int i = 0; i < len; i++) {
+		printf("%02x ", buf[i]);
+	}
+}
+
+void hex_dump_0(unsigned char *buf, int len) {
+	for (int i = 0; i < len; i++) {
+		if (buf[i] != 0) {
+			printf("%02x ", buf[i]);
+		}
+	}
+}
+
+void int_dump(unsigned char *buf, int len) {
+	for (int i = 0; i < len; i++) {
+		printf("%i ", buf[i]);
+	}
+	printf("\n");
+}
+
+int _floor(float n) {
+	return (int)n;
+}
+
+
+//void device_print(struct hid_device_info *dev) {
+//	printf("USB device info:\n  vid: 0x%04hX pid: 0x%04hX\n  path: %s\n  serial_number: %ls\n  interface_number: %d\n",
+//		dev->vendor_id, dev->product_id, dev->path, dev->serial_number, dev->interface_number);
+//	printf("  Manufacturer: %ls\n", dev->manufacturer_string);
+//	printf("  Product:      %ls\n\n", dev->product_string);
+//}
+
+
+
+//void print_dstick(Joycon *jc) {
+//	printf("%s\n", dstick_names[jc->dstick]);
+//}
+
+//inline bool exists_test0(const std::string& name) {
+//	ifstream f(name.c_str());
+//	return f.good();
+//}
+
+inline bool exists_test0(const std::string& name) {
+	if (FILE *file = fopen(name.c_str(), "r")) {
+		fclose(file);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+template<typename T>
+std::string get_time(std::chrono::time_point<T> time) {
+	using namespace std;
+	using namespace std::chrono;
+
+	time_t curr_time = T::to_time_t(time);
+	char sRep[100];
+	strftime(sRep, sizeof(sRep), "%Y-%m-%d %H:%M:%S", localtime(&curr_time));
+
+	typename T::duration since_epoch = time.time_since_epoch();
+	seconds sec = duration_cast<seconds>(since_epoch);
+	since_epoch -= sec;
+	milliseconds milli = duration_cast<milliseconds>(since_epoch);
+
+
+	string s = "";
+	stringstream ss;
+	ss << "[" << sRep << ":" << milli.count() << "]";
+	s = ss.str();
+
+	//s = to_string(sRep);
+
+	return s;
+
+}
