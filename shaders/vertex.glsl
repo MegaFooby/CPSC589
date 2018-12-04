53e8a6ebc06c07d05cc4e6314455c15f8d157400
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2018-12-03 17:10:25 -0700

    Add files via upload

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -8,17 +8,17 @@
 
 // location indices for these attributes correspond to those specified in the
 // InitializeGeometry() function of the main program
-layout(location = 0) in vec4 VertexPosition;
+layout(location = 0) in vec2 VertexPosition;
 layout(location = 1) in vec3 VertexColour;
 
 // output to be interpolated between vertices and passed to the fragment stage
-out vec3 tcColour;
+out vec3 Colour;
 
 void main()
 {
     // assign vertex position without modification
-    gl_Position = VertexPosition;
+    gl_Position = vec4(VertexPosition, 0.0, 1.0);
 
     // assign output colour to be interpolated
-    tcColour = VertexColour;
+    Colour = VertexColour;
 }
